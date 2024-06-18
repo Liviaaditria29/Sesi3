c97b3e2e79d530bbc8d09867f36f478f85bafe18
Livia Aditria <69287149+Liviaaditria29@users.noreply.github.com>
2024-06-18 23:54:42 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
+import plotly.express as px
 import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-import plotly.express as px
 import sklearn
 from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.ensemble import RandomForestClassifier
